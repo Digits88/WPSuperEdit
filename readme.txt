2a4fcf73b331aef269dff9adb871088b65006e56
Ahmad Awais <mrahmadawais@gmail.com>
2017-11-14 14:31:45 +0500

    Tested 💯

--- readme.txt
+++ readme.txt
@@ -1,9 +1,9 @@
-=== Plugin Name ===
-Contributors: mrahmadawais, ev3rywh3re
+=== WP Super Edit ===
+Contributors: mrahmadawais, maedahbatool, wptie, ev3rywh3re
 Donate link:
 Tags: TinyMCE, editor, wysiwyg, formatting, admin
 Requires at least: 4.2
-Tested up to: 4.7
+Tested up to: 4.9.0
 Stable tag: 2.5.3
 License: GPLv2 or later
 
@@ -22,16 +22,16 @@ Your feedback is always welcome!
 **Features**
 
 * Drag and Drop interface for arranging the WordPress visual editor buttons.
-* Access to built-in WordPress visual editor buttons and functions. 
+* Access to built-in WordPress visual editor buttons and functions.
 * Additional TinyMCE plugins to add buttons and features like tables, layers (div tag), advanced XHTML properties, advanced image and link properties, WordPress emoticons, style attributes, css classes for themes, search / replace, and more.
 * Options for allowing users to configure visual editor settings; One editor setting for all users, role based editor settings, and individual user editor settings. **Only WordPress administrators can activate or deactivate TinyMCE wysiwyg visual editor plugins. In single or role based modes, only administrators can arrange editor buttons.**
-* Easy to install and remove. WP Super Edit uses separate database tables for settings and to support multi-site configurations. Currently only the **Super Emoticon / Icon Plugin** will leave short tags in your posts or pages. 
+* Easy to install and remove. WP Super Edit uses separate database tables for settings and to support multi-site configurations. Currently only the **Super Emoticon / Icon Plugin** will leave short tags in your posts or pages.
 
 **Version Notice**
 
 This version has been tested for use with the versions of WordPress indicated. I attempt to keep WP Super Edit up to date with changes to WordPress and the visual editor, but the complex changes can make it unproductive to maintain compatiblity with some older versions of WordPress. This is a list of recent versions available for older WordPress sites.
 
-* Use WP Super Edit 2.1 for WordPress 2.6 to 2.7.1 
+* Use WP Super Edit 2.1 for WordPress 2.6 to 2.7.1
 * Use WP Super Edit 2.3.x for WordPress 2.8 to 3.1.x
 * Use WP Super Edit 2.4.x for WordPress 3.1 to 3.8.x
 
@@ -39,7 +39,7 @@ This version has been tested for use with the versions of WordPress indicated. I
 
 == Installation ==
 
-1. Take the whole **wp-super-edit** folder and put it in the **WordPress Plugin** directory for your Web site. 
+1. Take the whole **wp-super-edit** folder and put it in the **WordPress Plugin** directory for your Web site.
 2. Activate the plugin on the Wordpress Plugins administration panel
 3. Click on the Settings option and you will see WP Super Edit in the sub menu.
 4. WP Super Edit should lead you through the final installation steps. **Please be patient!** WP Super Edit will attempt to scan and save your original wysiwyg visual editor settings while installing. WP Super Edit will not work until the installation has been completed!
@@ -76,7 +76,7 @@ This version has been tested for use with the versions of WordPress indicated. I
 
 = 2.4.3 =
 * Using Andrew Ozz's method for loading language files inline. Fixes some issues with multisite and various js restrictions.
-* Introduce buttons and support for Distraction Free Writing. 
+* Introduce buttons and support for Distraction Free Writing.
 * Minor bug fixes to eliminate warnings
 
 = 2.4.2 =
@@ -103,7 +103,7 @@ This version has been tested for use with the versions of WordPress indicated. I
 = 2.3 =
 * WordPress 3.0.1 support (beta multi-site support)
 * Removing callback functionality, so additional TinyMCE external plugins can be added by building independent WordPress plugins.
-* uninstall.php to do the right thing when you delete this crap. 
+* uninstall.php to do the right thing when you delete this crap.
 * Moved Super-CSS-Classes and Super-Emoticons to separate plugins in this package.
 * A hopefully a better method of setting up default TinyMCE settings.
 
--- wp-super-edit.php
+++ wp-super-edit.php
@@ -3,13 +3,13 @@
 Plugin Name: WP Super Edit
 Plugin URI: http://funroe.net/projects/super-edit/
 Description: Get control of the WordPress wysiwyg visual editor and add some functionality with more buttons and customized TinyMCE plugins.
-Author: Jess Planck
+Author: WPCouple(Ahmad Awais & Maedah Batool)
 Version: 2.5.3
-Author URI: http://funroe.net
+Author URI: https://WPCouple.com/
 */
 
 /*
-Copyright (c) Jess Planck (http://funroe.net)
+Copyright (c) WPCouple (https://WPCouple.com/)
 WP Super Edit is released under the GNU General Public
 License: http://www.gnu.org/licenses/gpl.txt
 
@@ -36,11 +36,11 @@ Public License at http://www.gnu.org/copyleft/gpl.html
 */
 
 /**
-* WP Super Edit 
+* WP Super Edit
 *
 * These functions control the core functionality for this Wordpress Plugin. This
 * plugin is designed to extend and control the Wordpress visual WYSIWYG editor. The editor
-* is a javascript application known as TinyMCE provided by Moxicode AB. 
+* is a javascript application known as TinyMCE provided by Moxicode AB.
 * @package wp-super-edit
 * @author Jess Planck
 * @version 2.5.3
@@ -59,7 +59,7 @@ require_once( WP_PLUGIN_DIR . '/wp-super-edit/wp-super-edit.core.class.php' );
 /**
 * Conditional includes for WP Super Edit fuctions and classes in WordPress admin panels
 * Set $wp_super_edit primary object instance
-* @global object $wp_super_edit 
+* @global object $wp_super_edit
 */
 if ( is_admin() ) {
 	require_once( WP_PLUGIN_DIR . '/wp-super-edit/wp-super-edit.admin.class.php' );
@@ -75,12 +75,12 @@ do_action( 'wp_super_edit_loaded', 'wp_super_edit_loaded' );
 *
 * This function is initializes WP Super Edit. We add it to wp_loaded so you have time to remove it or interact with it.
 *
-* @global object $wp_super_edit 
+* @global object $wp_super_edit
 */
 function wp_super_edit_init() {
 	global $wp_super_edit;
-	
-	$wp_super_edit_run_mode = 'off';	
+
+	$wp_super_edit_run_mode = 'off';
 	if ( is_admin() ) $wp_super_edit_run_mode = 'admin';
 	$wp_super_edit_run_mode = apply_filters( 'wp_super_edit_run_mode',  $wp_super_edit_run_mode );
 
@@ -93,12 +93,12 @@ function wp_super_edit_init() {
 	// WP Super Edit Administration interfaces and default manipulation of TinyMCE.
 	case 'admin':
 		$wp_super_edit = new wp_super_edit_admin();
-		
+
 		if ( !$wp_super_edit->is_installed ) require_once( $wp_super_edit->core_path . 'wp-super-edit-defaults.php' );
-		
+
 		do_action( 'wp_super_edit_mode_admin', 'wp_super_edit_mode_admin' );
 		add_action('admin_menu', 'wp_super_edit_admin_menu_setup');
-		add_action('admin_init', 'wp_super_edit_admin_setup');		
+		add_action('admin_init', 'wp_super_edit_admin_setup');
 		add_filter('mce_external_plugins','wp_super_edit_tinymce_plugin_filter', 99);
 		add_filter('mce_external_languages','wp_super_edit_tinymce_plugin_lang_filter', 99);
 		add_filter('tiny_mce_before_init','wp_super_edit_tinymce_filter', 99);
@@ -113,11 +113,11 @@ add_action( 'plugins_loaded', 'wp_super_edit_init' );
 *
 * This function is a WordPress filter designed to replace the TinyMCE configuration array
 * with the configuration array created by WP Super Edit.
-* @global object $wp_super_edit 
+* @global object $wp_super_edit
 */
 function wp_super_edit_tinymce_filter( $initArray ) {
 	global $wp_super_edit;
-	
+
 	//ISSUE:: Find out what TinyMCE is spewing
 	//echo "ISSUE::";
 	//print_r( $initArray );
@@ -125,28 +125,28 @@ function wp_super_edit_tinymce_filter( $initArray ) {
 	if ( !$wp_super_edit->is_installed ) return $initArray;
 
 	$initArray = $wp_super_edit->tinymce_settings( $initArray );
-	
+
 	$initArray['relative_urls'] = false;
 	$initArray['remove_script_host'] = false;
 	$initArray['convert_urls'] = false;
-	
+
 	return $initArray;
 }
 
 /**
 * WP Super Edit TinyMCE Plugin filter
 *
-* This WordPress filter passes plugins activated by WP Super Edit and passes them during init of 
+* This WordPress filter passes plugins activated by WP Super Edit and passes them during init of
 * TinyMCE.
-* @global object $wp_super_edit 
+* @global object $wp_super_edit
 */
 function wp_super_edit_tinymce_plugin_filter( $tinymce_plugins ) {
 	global $wp_super_edit;
-		
+
 	if ( !$wp_super_edit->is_installed ) return $tinymce_plugins;
-	
+
 	if ( !is_array( $wp_super_edit->plugins ) ) return;
-	
+
 	foreach( $wp_super_edit->plugins as $plugin ) {
 		if ( $plugin->status != 'yes' ) continue;
 
@@ -156,38 +156,38 @@ function wp_super_edit_tinymce_plugin_filter( $tinymce_plugins ) {
 			} else {
 				if ( $plugin->url != 'none' ) $tinymce_plugins[$plugin->name] = plugins_url() . $plugin->url;
 			}
-		} else { 
+		} else {
 			// ISSUE: plugin files are now plugin.js
 			if ( in_array( $plugin->provider, $wp_super_edit->providers_registered ) ) $tinymce_plugins[$plugin->name] = $wp_super_edit->core_uri . 'tinymce_plugins/' . $plugin->name . '/plugin.js';
 		}
 	}
-	
+
 	//echo 'ISSUE:: TinyMCE Plugins have slashes </br>';
 	//print_r( $tinymce_plugins );
-	
+
 	return $tinymce_plugins;
 }
 
 if ( ! function_exists('wp_super_edit_get_file') ) {
 	function wp_super_edit_get_file($path) {
-	
+
 		if ( function_exists('realpath') )
 			$path = realpath($path);
-	
+
 		if ( ! $path || ! @is_file($path) )
 			return '';
-	
+
 		if ( function_exists('file_get_contents') )
 			return @file_get_contents($path);
-	
+
 		$content = '';
 		$fp = @fopen($path, 'r');
 		if ( ! $fp )
 			return '';
-	
+
 		while ( ! feof($fp) )
 			$content .= fgets($fp);
-	
+
 		fclose($fp);
 		return $content;
 	}
@@ -197,24 +197,24 @@ if ( ! function_exists('wp_super_edit_get_file') ) {
 /**
 * WP Super Edit TinyMCE Plugin language filter
 *
-* This WordPress filter passes plugin language files if activated by WP Super Edit. The /langs/lang.php file builds 
+* This WordPress filter passes plugin language files if activated by WP Super Edit. The /langs/lang.php file builds
 * string pairs suitable for the WordPress method of initializing TinyMCE.
-* @global object $wp_super_edit 
+* @global object $wp_super_edit
 */
 function wp_super_edit_tinymce_plugin_lang_filter( $tinymce_langs ) {
 	global $wp_super_edit;
-		
+
 	if ( !$wp_super_edit->is_installed ) return $tinymce_langs;
-	
+
 	if ( !is_array( $wp_super_edit->plugins ) ) return;
-	
+
 	$mce_locale = ( '' == get_locale() ) ? 'en' : strtolower( substr(get_locale(), 0, 2) ); // only ISO 639-1
-	
+
 	foreach( $wp_super_edit->plugins as $plugin ) {
 		if ( $plugin->status != 'yes' ) continue;
 
 		if ( $plugin->url != '' ) {
-			/* 
+			/*
 			ISSUE: Not sure how I'll get his to work. External Plugin URLS and Paths need to be built.
 			External files are really sensitive since TinyMCE loads them in JS. SSL definitely fails here.
 			if ( preg_match("/^(http:|https:)/i", $plugin->url ) ) {
@@ -223,28 +223,28 @@ function wp_super_edit_tinymce_plugin_lang_filter( $tinymce_langs ) {
 				if ( $plugin->url != 'none' ) $tinymce_plugins[$plugin->name] = plugins_url() . $plugin->url;
 			}
 			*/
-		} else { 
+		} else {
 			// ISSUE: Stolen from TinyMCE Advanced because it makes sense inline or languages will fail on SSL.
 			if ( file_exists( $wp_super_edit->core_path . 'tinymce_plugins/' . $plugin->name . '/langs/langs.php' ) ) {
 				if ( in_array( $plugin->provider, $wp_super_edit->providers_registered ) ) $tinymce_langs[$plugin->name] = $wp_super_edit->core_path . 'tinymce_plugins/' . $plugin->name . '/langs/langs.php';
 			}
 		}
 	}
-	
+
 	return $tinymce_langs;
 }
 
-// ISSUE: Placeholder for json data migrations 
+// ISSUE: Placeholder for json data migrations
 function wp_super_edit_data_encode( $data ) {
 
 }
 
-// ISSUE: Placeholder for json data migrations 
+// ISSUE: Placeholder for json data migrations
 function wp_super_edit_data_decode( $data ) {
 
 }
 
-// ISSUE: Placeholder for json data migrations 
+// ISSUE: Placeholder for json data migrations
 function wp_super_edi_is_json( $string ) {
 	json_decode($string);
 	return (json_last_error() == JSON_ERROR_NONE);
